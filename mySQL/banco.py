7c433351f27f89de25a531f6b13bbe8e3d3e00e6
João Victor Benedet Machado <jvbm2177@gmail.com>
2024-10-08 14:57:12 -0300

    CRUD com MySQL

--- mySQL/banco.py
+++ mySQL/banco.py
@@ -21,23 +21,66 @@ def InsertDB(nome, valor):
   cursor.execute(comandoSQL)
   DBconexao.commit()
   print(f"Criado {nome} com valor de {valor} com sucesso")
-  
 
-nomeproduto = input("Digite o nome do novo produto: ")
-valorproduto = float(input("Digite o valor do novo produto: "))
+# CRUD - READ
 
-InsertDB(nomeproduto, valorproduto)
-
+def ReadDB():
+  comandoSQL = f"SELECT * FROM produtos"
+  cursor.execute(comandoSQL)
+  resultado = cursor.fetchall() 
+  print()
+  for i in resultado:
+    print(f"{i[0]:<5}|{i[1]:<15}|{i[2]:<10.2f}")
+  print()
+    
 # CRUD - UPDATE
 
 def UpdateDB(tabela, novoValor, condicao):
-  comandoSQL = f'UPDATE produtos SET nomeproduto = "{novoValor}" where idproduto = {condicao}'
+  comandoSQL = f'UPDATE produtos SET {tabela} = "{novoValor}" where idproduto = {condicao}'
   cursor.execute(comandoSQL)
   DBconexao.commit()
+  print(f"Alterado com sucesso")
   
-tabela = input("Qual tabela deseja alterar [nomeproduto / valorproduto]?:  ")
-condicao = int(input("Digite o id que deseja alterar: "))
-novoValor = input(f"Novo valor para o {condicao}: ").title()
+# CRUD - DELETE
 
-UpdateDB(tabela, novoValor, condicao)
+def DeleteDB(id):
+  comandoSQL = f"DELETE FROM produtos WHERE idproduto = {id}"
+  cursor.execute(comandoSQL)
+  DBconexao.commit()
+  print(f"ID: {id} Excluido com sucesso")
 
+
+while True:
+  print("1 - Inserir produto")
+  print("2 - Alterar produto")
+  print("3 - Excluir produto")
+  print("4 - Ler produto")
+
+  escolha = input("> ")
+  
+  if escolha == "1":
+    nomeproduto = input("Digite o nome do novo produto: ")
+    valorproduto = float(input("Digite o valor do novo produto: "))
+    InsertDB(nomeproduto, valorproduto)
+    
+  if escolha == "2":
+    tabela = input("Qual tabela deseja alterar [nomeproduto / valorproduto]?:  ")
+    condicao = int(input("Digite o id que deseja alterar: "))
+    
+    comandoSQL = f"SELECT * FROM produtos"
+    cursor.execute(comandoSQL)
+    resultado = cursor.fetchall() 
+    
+    if condicao in resultado[0]:
+      # print("existe")
+      novoValor = input(f"Novo valor para o {condicao}: ").title()
+      UpdateDB(tabela, novoValor, condicao)
+    else:
+      print(f"ID {id} não encontrado")
+    
+  if escolha == "3":
+    idproduto = int(input("Digite o id do produto que deseja excluir: "))
+    DeleteDB(idproduto)
+  
+  if escolha == "4":
+    ReadDB()
